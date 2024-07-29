de6fdfa721c1e6e1343a306a256590aa5379fee6
stanislavets <stanislavets2009@mail.ru>
2024-07-29 18:30:47 +0300

    Add files via upload

---
+++ module_3_2 корр.py
@@ -0,0 +1,16 @@
+def send_email(message, recipient, *, sender="university.help@gmail.com"):
+    if sender == recipient:
+        print("Нельзя отправить письмо самому себе!")
+    elif "@" not in sender or not (sender.endswith(".com") or sender.endswith(".ru") or sender.endswith(".net")):
+        print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
+    else:
+        if sender == "university.help@gmail.com":
+            print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
+        else:
+            print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
+
+# Тестирование функции
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
